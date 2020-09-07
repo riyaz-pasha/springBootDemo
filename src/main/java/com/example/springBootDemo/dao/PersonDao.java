c2163a1380d4d6b4402d750bd0af7dcd62ea825d
riyaz <riyazpasha880@gmail.com>
2020-09-07 10:21:41 +0530

    create basic person model and fake person acess db

---
+++ src/main/java/com/example/springBootDemo/dao/PersonDao.java
@@ -0,0 +1,14 @@
+package com.example.springBootDemo.dao;
+
+import com.example.springBootDemo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
